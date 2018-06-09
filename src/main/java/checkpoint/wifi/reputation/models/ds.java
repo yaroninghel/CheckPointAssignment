1f7a41b9dc92cfa6e5ac085e3e419a34e7299624
Yaron Inghel <yaron.inghel@gmail.com>
2018-06-09 13:05:28 +0300

    FIRST

---
+++ src/main/java/checkpoint/wifi/reputation/models/ds.java
@@ -0,0 +1,9 @@
+package checkpoint.wifi.reputation.models;
+
+import checkpoint.wifi.reputation.enums.AuthType;
+
+public class ds {
+    public String device_id;
+    public String network_id;
+    public AuthType auth;
+}
